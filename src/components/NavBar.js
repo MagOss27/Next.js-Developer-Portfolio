f2d181182d2927418333a32c7650381f83d5848c
MagOss27 <MagnusOss98@gmail.com>
2025-04-09 02:11:20 -0300

    fix: icones do NavBar criados

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,15 +2,18 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
 import { useRouter } from 'next/router'
+import { TwitterIcon, DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon } from './Icons'
+import { motion } from "framer-motion"
 
 const CustomerLink = ({ href, title, className = "" }) => {
-    const router = useRouter()
-    console.log(router)
-    return (
-        <Link href={href} >
-            {title}className={`${className} relative group`}
+    const router = useRouter();
 
-            <span className='h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300'>&nbsp;</span>
+    return (
+        <Link href={href}>
+            <span className={`${className} relative group`}>
+                {title}
+                <span className={`h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? "w-full" : "w-0"}`}>&nbsp;</span>
+            </span>
         </Link>
     )
 }
@@ -27,16 +30,24 @@ const NavBar = () => {
                 <CustomerLink href="/articles" title="Articles" className='ml-4' />
             </nav>
 
-            <nav>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
+            <nav className='flex items-center justify-center flex-wrap'>
+                <motion.a href="https://twitter.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3">
+                    <TwitterIcon />
+                </motion.a >
+                <motion.a href="https://twitter.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3">
+                    <GithubIcon />
+                </motion.a >
+                <motion.a href="https://twitter.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3">
+                    <LinkedInIcon />
+                </motion.a >
+                <motion.a href="https://twitter.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 mx-3">
+                    <PinterestIcon />
+                </motion.a >
+                <motion.a href="https://twitter.com" target={"_blank"} whileHover={{ y: -2 }} whileTap={{ scale: 0.9 }} className="w-6 ml-3">
+                    <DribbbleIcon />
+                </motion.a >
             </nav>
-            <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
+            <div className='absolute left-[50%] top-2 translate-x-[-50%]' >
                 <Logo />
             </div>
         </header>
