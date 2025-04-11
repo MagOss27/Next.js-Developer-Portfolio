0e9339bdd991fc80cb355051aa24e5e512d551ec
MagOss27 <MagnusOss98@gmail.com>
2025-04-11 00:37:55 -0300

    fix: componentes HireMe e Footer adicionados no index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import profilePic from "../../public/images/profile/developer-pic-1.png"
 import AnimatedText from '@/components/AnimatedText'
+import Link from 'next/link'
+import { LinkArrow } from '@/components/Icons'
+import HireMe from '@/components/HireMe'
+import lightBuld from "../../public/images/svgs/lanterna.svg";
 
 export default function Home() {
   return (
@@ -20,13 +24,21 @@ export default function Home() {
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6x !text-left' />
-              <p>
+              <p className='my-4 text-base font-medium'>
                 As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
                 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
+              <div className='flex items-center self-start mt-2'>
+                <Link href="/CurriculoProfissional-Atualizado.pdf" target={"_blank"} className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border-2 border-solid border-transparent hover:border-dark' download={true}>Resume <LinkArrow className={"w-6 ml-1"} /> </Link>
+                <Link href="mailto:MagnusOss98@gmail.com" target={"_blank"} className='ml-4 text-lg font-medium capitalize text-dark underline'>Contact</Link>
+              </div>
             </div>
           </div>
         </Layout>
+        <HireMe />
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightBuld} alt="Codebucks" className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
