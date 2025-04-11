6d5d551d5a564b413cff3a7f22149d5ce2ac3390
MagOss27 <MagnusOss98@gmail.com>
2025-04-11 17:37:35 -0300

    fix: criação do about.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,8 @@ export default function Home() {
         <Layout className='pt-0'>
           <div className='flex items-center justify-between w-full'>
             <div className='w-1/2'>
-              <Image src={profilePic} alt='CodeBucks' className='w-full h-auto' />
+              <Image src={profilePic} alt='CodeBucks'     className='w-full h-auto max-w-sm' 
+ />
             </div>
             <div className='w-1/2 flex flex-col items-center self-center'>
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6x !text-left' />
