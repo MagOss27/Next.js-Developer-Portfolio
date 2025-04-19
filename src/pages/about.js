e0fe24d4d20d4868fd47bea69943a64fa0aa20b6
MagOss27 <MagnusOss98@gmail.com>
2025-04-18 22:50:22 -0300

    fix: finalização a primeira parte do projeto

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,13 +8,13 @@ import { useInView, useMotionValue, useSpring } from 'framer-motion'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
 import Education from '@/components/Education'
+import TransitionEffect from '@/components/TransitionEffect'
 
 const AnimatedNumbers = ({ value }) => {
     const ref = useRef(null);
-
     const motionValue = useMotionValue(0);
     const springValue = useSpring(motionValue, { duration: 3000 });
-    const isInView = useInView(ref, { once: true }); // Aqui tá o fix!
+    const isInView = useInView(ref, { once: true });
 
     useEffect(() => {
         if (isInView) {
@@ -33,61 +33,69 @@ const AnimatedNumbers = ({ value }) => {
     return <span ref={ref}></span>;
 };
 
-
-function about() {
+function About() {
     return (
         <>
             <Head>
                 <title>CodeBucks | About Page</title>
                 <meta name='description' content="any description" />
             </Head>
-            <main className="flex w-full flex-col items-center justify-center dark:text-light ">
+            <TransitionEffect />
+            <main className="flex w-full flex-col items-center justify-center dark:text-light">
                 <Layout className='pt-16'>
-                    <AnimatedText text="Passion Fuels Purpose!" className='mb-16' />
-                    <div className='grid w-full grid-cols-8 gap-16'>
-                        <div className='col-span-3 flex flex-col items-start justify-start'>
-                            <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
+                    <AnimatedText text="Passion Fuels Purpose!" className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
 
-                            <p className='font-medium'>Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional,
+                    <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+                        {/* BIO */}
+                        <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8'>
+                            <h2 className='mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75'>Biography</h2>
+                            <p className='font-medium'>
+                                Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional,
                                 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for
                                 new and innovative ways to bring my clients' visions to life.
                             </p>
-
-                            <p className=' my-4 font-medium'>I believe that design is about more than just making things look pretty – it's about solving problems and
+                            <p className='my-4 font-medium'>
+                                I believe that design is about more than just making things look pretty – it's about solving problems and
                                 creating intuitive, enjoyable experiences for users.
                             </p>
-
-                            <p className='font-medium'> Whether I'm working on a website, mobile app, or
+                            <p className='font-medium'>
+                                Whether I'm working on a website, mobile app, or
                                 other digital product, I bring my commitment to design excellence and user-centered thinking to
-                                every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
+                                every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
+                            </p>
                         </div>
-                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light'>
+
+                        {/* PROFILE PIC */}
+                        <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'>
                             <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-2xl bg-dark dark:bg-light' />
-                            <Image src={profilePic} alt='Codebucks' className='w-full h-auto rounded-2xl'
+                            <Image
+                                src={profilePic}
+                                alt='Codebucks'
+                                className='w-full h-auto rounded-2xl'
                                 priority
                                 sizes='(max-width: 768px) 100vw,
-                           (max-width:1200px) 50vw,
-                           33vw'
+                                        (max-width:1200px) 50vw,
+                                        33vw'
                             />
                         </div>
 
-                        <div className='col-span-2 flex flex-col items-end justify-between'>
-                            <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={50} />+</span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75 '>satisfied clients</h2>
+                        {/* STATS */}
+                        <div className='col-span-2 flex flex-col items-end justify-between gap-6 xl:col-span-8 xl:flex-row xl:items-center md:order-3 md:flex-row md:justify-around md:items-center sm:flex-col sm:items-center sm:gap-8'>
+                            <div className='flex flex-col items-end sm:items-center'>
+                                <span className='inline-block text-5xl sm:text-6xl font-bold'><AnimatedNumbers value={50} />+</span>
+                                <h2 className='text-lg sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75'>satisfied clients</h2>
                             </div>
 
-                            <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={40} />+</span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>projects completed</h2>
+                            <div className='flex flex-col items-end sm:items-center'>
+                                <span className='inline-block text-5xl sm:text-6xl font-bold'><AnimatedNumbers value={40} />+</span>
+                                <h2 className='text-lg sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75'>projects completed</h2>
                             </div>
 
-                            <div className='flex flex-col items-end justify-center'>
-                                <span className='inline-block text-7xl font-bold'><AnimatedNumbers value={4} />+</span>
-                                <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
+                            <div className='flex flex-col items-end sm:items-center'>
+                                <span className='inline-block text-5xl sm:text-6xl font-bold'><AnimatedNumbers value={4} />+</span>
+                                <h2 className='text-lg sm:text-xl font-medium capitalize text-dark/75 dark:text-light/75'>years of experience</h2>
                             </div>
                         </div>
-
                     </div>
 
                     <Skills />
@@ -99,4 +107,4 @@ function about() {
     )
 }
 
-export default about
+export default About
--- src/pages/articles.js
+++ src/pages/articles.js
@@ -9,6 +9,7 @@ import article2 from '../../public/images/articles/create loading screen in reac
 import { motion, useMotionValue } from 'framer-motion';
 import article3 from "../../public/images/articles/modal.png"
 import article4 from "../../public/images/articles/form.png"
+import TransitionEffect from '@/components/TransitionEffect';
 
 const FramerImage = motion(Image);
 
@@ -36,12 +37,12 @@ const MovingImg = ({ title, img, link }) => {
             onMouseMove={handleMouse}
             onMouseLeave={handleMouseLeave}
         >
-            <h2 className='capitalize text-xl font-semibold hover:underline'>{title}</h2>
+            <h2 className='capitalize text-xl font-semibold hover:underline '>{title}</h2>
             <FramerImage
                 style={{ x: x, y: y }}
                 initial={{ opacity: 0 }}
                 whileInView={{ opacity: 1, transition: { duration: 0.2 } }}
-                ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg' />
+                ref={imgRef} src={img} alt={title} className='z-10 w-96 h-auto hidden absolute rounded-lg md:!hidden' />
         </Link>
     );
 }
@@ -52,9 +53,11 @@ const Article = ({ img, title, date, link }) => {
             initial={{ y: 200 }}
             whileInView={{ y: 0, transition: { duration: 0.5, ease: "easeInOut" } }}
             viewport={{ once: true }}
-            className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light'>
+            className='relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between bg-light text-dark first:mt-0 border border-solid border-dark border-r-4 border-b-4 dark:border-light dark:bg-dark dark:text-light
+            sm:flex-col 
+            '>
             <MovingImg title={title} img={img} link={link} />
-            <span className='text-primary font-semibold pl-4 dark:text-primaryDark'>{date}</span>
+            <span className='text-primary font-semibold pl-4 dark:text-primaryDark sm:self-start sm:pl-0 xs:text-sm'>{date}</span>
         </motion.li>
     );
 }
@@ -76,7 +79,7 @@ const FeaturedArticle = ({ img, title, time, summary, link }) => {
                 />
             </Link>
             <Link href={link} target='_blank'>
-                <h2 className='mt-4 capitalize text-2xl font-bold my-2 hover:underline'>{title}</h2>
+                <h2 className='mt-4 capitalize text-2xl font-bold my-2 hover:underline xs:text-lg'>{title}</h2>
             </Link>
             <p className='text-sm mb-2'>{summary}</p>
             <span className='text-primary font-semibold dark:text-primaryDark'>{time}</span>
@@ -91,10 +94,14 @@ function Articles() {
                 <title>CodeBucks | Articles Page</title>
                 <meta name='description' content='any description' />
             </Head>
+            <TransitionEffect />
             <main className='w-full mb-16 flex flex-col items-center justify-center overflow-hidden dark:text-light'>
                 <Layout className='pt-16'>
-                    <AnimatedText text='Words Can Change The World!' className='mb-16' />
-                    <ul className='grid grid-cols-2 gap-16'>
+                    <AnimatedText text='Words Can Change The World!' className='mb-16
+                    lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl
+                    '
+                    />
+                    <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16'>
                         <FeaturedArticle
                             title='Build A Custom Pagination Component In Reactjs From Scratch'
                             summary='Learn how to build a custom pagination component in ReactJS from scratch. 
